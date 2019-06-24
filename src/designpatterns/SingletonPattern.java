e7a2dcdf776d4626a1b90cd0878e5c5e6c745f27
RavinduW <raviduchinthaka3@gmail.com>
2019-06-24 19:02:16 +0530

    comments

--- src/designpatterns/SingletonPattern.java
+++ src/designpatterns/SingletonPattern.java
@@ -9,6 +9,38 @@ package designpatterns;
  *
  * @author Ravindu Weerasinghe
  */
+
+//singleton pattern - a class must ensure that only single instance should be created
+// and single object can be access by all other classes
+
+//there are 2 types of singleton pattern
+// 1. early instantiation - creating the instance at load time
+// 2. lazy instantiation - creating the instance when it is needed
+
+//advantages of singleton patterns =>
+//saves memory bcz object creation doesn't need everytime.. object instance can be reuse again and again
+
+//singleton pattern is used in multi threaded and database applications
+
 public class SingletonPattern {
     
+    //defines the static member
+    private static SingletonPattern sp =new SingletonPattern();
+    
+    //defines private constructor
+    private SingletonPattern(){}
+    
+    //defines static factory method
+    public static SingletonPattern getObjectInstance(){
+        return sp;
+    }
+    
+    public static void main(String args[]){
+        
+    }    
 }
+
+//why sp is static? bcz it gets memory only once and it contains instance of the Singleton object
+//why constructor is private? bcz it will prevent instantiate Singleton class outside of the class
+//static factory method? to provide global access point to the singleton object and returns the instance when the method is called
+
